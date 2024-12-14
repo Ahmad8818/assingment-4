501b0690265ffbe6da5803127047cced0ab402b3
Ahmad8818 <ahmad8818510@gmail.com>
2024-12-14 19:32:16 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,83 @@
+let birthyear =2004
+let currentyear =2024
+let age=  currentyear-birthyear
+console.log(  "age =" + " " + age)
+
+
+let len=10
+let wir=20
+let area=len*wir
+console.log( "area of a rectangle=" + " " + area)
+
+
+let rad=10
+let circle=3.1416*10**2
+console.log( "area of a circle =" + " " +  circle)
+
+
+let cube=6*(10)**2
+console.log ("area of a cube =" + " " +  cube)
+
+
+let celsius= (10-32)*5/9
+console.log(  " temperature from Fahrenheit to Celsius = " + " " +celsius)
+
+
+let min= 100/60
+console.log("sec into min =" + " " + min)
+
+
+let per=900/1100*100
+console.log("percentage =" + " " + per)
+
+
+let a = 2;
+let b = ++a * 2; 
+console.log(b)
+
+
+
+
+let x = 5;
+let y = x-- + 2;
+console.log(y)
+
+
+
+let z = 3;
+let k = ++x + x++ + ++x;
+console.log(k)
+
+
+let m = 2;
+let n = ++m * m++ * --m;
+console.log(n)
+
+
+let c = 3;
+let d = 5;
+let result = ++c + d-- - c++ + --d;
+console.log(result)
+
+
+let e = 2;
+let f = 4;
+let p = e++ + ++f - --e + f--;
+console.log(p)
+
+
+
+let g = 5;
+let h = 3;
+let i = 2;
+let j = 7;
+
+let resul = ++g * (h-- + i) / --j;
+console.log(resul)
+
+
+let q = 2;
+let r = 3;
+let o = 4;
+let res= q++ * (--r + q) / (o-- - r);
+console.log(res)
